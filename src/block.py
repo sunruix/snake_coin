dd9bc0f9bf3f8f830cc5dfba095294741f085d1f
sunrui <icarux@126.com>
2018-04-09 08:52:15 +0800

    first commitment

---
+++ src/block.py
@@ -0,0 +1,23 @@
+'''
+Created on 2018年4月3日
+
+@author: g
+'''
+import hashlib as hasher
+
+class Block():
+    def __init__(self, index, timestamp, data, previous_hash):
+        self.index = index;
+        self.timestamp = timestamp
+        self.data = data
+        self.previous_hash = previous_hash
+        self.hash = self.hash_block()
+
+    def hash_block(self):
+        sha = hasher.sha256()
+        sha.update((str(self.index) +
+                   str(self.timestamp) +
+                   str(self.data) +
+                   str(self.previous_hash)).encode())
+        return sha.hexdigest()
+        
